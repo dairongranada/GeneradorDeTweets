5528afa580a152c8125a4360f2f61b0a910a9419
Dairon Camargo Granada <93607018+dairongranada@users.noreply.github.com>
2022-04-27 11:40:04 -0500

    Se agrego el Generador De Tweets.DG

---
+++ Generador de Tweet/js/app.js
@@ -0,0 +1,48 @@
+
+
+
+
+        // LLAMADO A LOS IDS
+
+// LETRA EN EL DIV DE ARRIBA
+const input = document.getElementById('inputLetter');
+const divParraf = document.getElementById('Parraf');
+
+//IMPRIMIR CARACTERES
+const texTarea = document.getElementById('idTextarea');
+const parraftRint = document.getElementById('ParraFinal');
+
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click', caPrint)
+
+// F U N C I O N   D E L   B O T O N
+function caPrint(eventPrint) {
+    if (eventPrint.type ==  "click"){
+        parraftRint.textContent = texTarea.value;
+        divParraf.textContent = input.value.charAt(0).toUpperCase();
+        
+    }
+}
+
+
+
+
+//CONTADOR
+function contador() {
+    const counterParraf = document.getElementById('counter');
+    let valor = 200;
+    let cantidad = texTarea.value.length;
+    document.getElementById("counter").textContent =   `Caracteres: ${valor - cantidad}`;
+    
+    if (cantidad > 150) {
+        counter.style.color = "red";
+    }else{
+        counter.style.color = "white";
+    }
+}
+
+
+
+
